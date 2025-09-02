1ee31ab768b66ac65ce412661f6057826bf07ef0
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2025-09-03 00:05:16 +0530

    Time: 0 ms (100%), Space: 18.6 MB (28.81%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,38 +11,20 @@
  */
 class Solution {
 public:
-    bool isSymmetric(TreeNode* root) {
-        queue<pair<int , TreeNode*>> q1;
-        queue<pair<int , TreeNode*>> q2;
-        if(root->left) q1.push({0 , root->left});
-        if(root->right) q2.push({0 , root->right});
-
-        while(!q1.empty() || !q2.empty()){
-            int size1 = q1.size();
-            int size2 = q2.size();
-
-            if(size1 != size2){
-                return false;
-            }
-            
-            for(int i = 0; i<size1; i++){
-                TreeNode* temp1 = q1.front().second;
-                int lx = q1.front().first;
-                q1.pop();
-                TreeNode* temp2 = q2.front().second;
-                int ly = q2.front().first;
-                q2.pop();
-
-                if(temp1->val == temp2->val && lx == ly){
-                    if(temp1->left) q1.push({lx-1 , temp1->left});
-                    if(temp1->right) q1.push({lx+1 , temp1->right});
-                    if(temp2->right) q2.push({ly-1 , temp2->right});
-                    if(temp2->left) q2.push({ly+1 , temp2->left});
-                }else{
-                    return false;
-                }
-            }
+    bool helper(TreeNode* temp1 , TreeNode* temp2){
+        if(!temp1 && !temp2){
+            return true;
         }
-        return true;
+        if(!temp1 || !temp2){
+            return false;
+        }
+        return (temp1->val == temp2->val) &&
+                helper(temp1->left , temp2->right) &&
+                helper(temp1->right , temp2->left);
+
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return helper(root->left , root->right);;
     }
 };
