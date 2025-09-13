4e1e98dff815ca5464b3d9298e21ba7fca7032a3
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2025-09-13 16:38:40 +0530

    Time: 0 ms (100%), Space: 17.9 MB (9.28%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,50 +11,20 @@
  */
 class Solution {
 public:
+    void flattenhelper(TreeNode* root , vector<TreeNode*> &preOrder){
+        if(!root) return;
+        preOrder.push_back(root);
+        flattenhelper(root->left , preOrder);
+        flattenhelper(root->right , preOrder);
+    }
     void flatten(TreeNode* root) {
         if(!root) return;
-        TreeNode* node = root;
-        stack<TreeNode*> s1;
-        stack<TreeNode*> s2;
-        if(root->left) s1.push(root->left);
-        if(root->right) s2.push(root->right);
-        node->left = nullptr;
-        node->right = nullptr;
-
-        while(!s1.empty()){
-            int size = s1.size();
-            for(int i = 0; i<size; i++){
-                TreeNode* temp = s1.top();
-                s1.pop();
-                node->right = temp;
-                node->left = nullptr;
-                node = node->right;
-
-                if(temp->left){
-                    s1.push(temp->left);
-                }
-                if(temp->right){
-                    s2.push(temp->right);
-                }
-            }
-        }
-
-        while(!s2.empty()){
-            int size = s2.size();
-            for(int i = 0; i<size; i++){
-                TreeNode* temp = s2.top();
-                s2.pop();
-                node->right = temp;
-                node->left = nullptr;
-                node = node->right;
-
-                if(temp->right){
-                    s2.push(temp->right);
-                }
-                if(temp->left){
-                    s2.push(temp->left);
-                }
-            }
+        vector<TreeNode*> preOrder;
+        flattenhelper(root , preOrder);
+        int len = preOrder.size()-1;
+        for(int i = 0; i<len; i++){
+            preOrder[i]->right = preOrder[i+1];
+            preOrder[i]->left = nullptr;
         }
 
     }
