f227b74c3f8fbd40b5bc36b5c90d376ad30149cc
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2025-11-18 14:38:35 +0530

    Time: 4 ms (29.5%), Space: 20.7 MB (35.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
-    void dfs(int node , vector<vector<int>> &adj , vector<int> &visited){
-        visited[node] = 1;
-        for(int i = 0; i<adj[node].size(); i++){
-            if(!visited[adj[node][i]]){
-                dfs(adj[node][i] , adj , visited);
+    void bfs(int node , vector<vector<int>> &adj , vector<int> &visited){
+        queue<int> q;
+        q.push(node);
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int i = 0; i<adj[node].size(); i++){
+                if(!visited[adj[node][i]]){
+                    visited[adj[node][i]] = 1;
+                    q.push(adj[node][i]);
+                }
             }
         }
     }
@@ -25,7 +32,7 @@ public:
         for(int i = 0; i<V; i++){
             if(visited[i] != 1){
                 count++;
-                dfs(i , adj , visited);
+                bfs(i , adj , visited);
             }
         }
 
