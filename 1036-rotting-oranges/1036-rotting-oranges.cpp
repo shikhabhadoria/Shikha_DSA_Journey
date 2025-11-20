dea65e8681b943999fdedc13cc006ca075cc5df1
shikha bhadoria <145272515+shikhabhadoria@users.noreply.github.com>
2025-11-20 11:00:36 +0530

    Time: 4 ms (16.7%), Space: 17 MB (54.3%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,13 +3,14 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int , int>> q;
         int fresh = 0;
+        queue<pair<int , int>> q;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(grid[i][j] == 2){
                     q.push({i , j});
                 }
+
                 if(grid[i][j] == 1){
                     fresh++;
                 }
@@ -17,45 +18,24 @@ public:
         }
 
         if(fresh == 0) return 0;
+        vector<vector<int>> v = {{-1 , 0} , {0 , 1} , {1 , 0} , {0 , -1}};
         int timer = 0;
         while(!q.empty()){
             int size = q.size();
             bool flag = false;
-
-            for(int p = 0; p<size; p++){
-                int i = q.front().first;
-                int j = q.front().second;
+            for(int i = 0; i<size; i++){
+                int x = q.front().first;
+                int y = q.front().second;
                 q.pop();
 
-                if(i-1 >= 0 && j < n){
-                    if(grid[i-1][j] == 1){
-                        q.push({i-1 , j});
-                        grid[i-1][j] = 2;
-                        flag = true;
-                    }
-                }
+                for(int j = 0; j<4; j++){
+                    int dx = x + v[j][0];
+                    int dy = y + v[j][1];
 
-                if(i < m && j+1 < n){
-                    if(grid[i][j+1] == 1){
-                        q.push({i , j+1});
-                        grid[i][j+1] = 2;
-                        flag = true;
-                    }
-                }
-
-                if(i+1 < m && j < n){
-                    if(grid[i+1][j] == 1){
-                        q.push({i+1 , j});
-                        grid[i+1][j] = 2;
-                        flag = true;
-                    }
-                }
-
-                if(i < m && j-1 >= 0){
-                    if(grid[i][j-1] == 1){
-                        q.push({i , j-1});
-                        grid[i][j-1] = 2;
-                        flag = true;
+                    if(dx >= 0 && dx < m && dy >= 0 && dy < n && grid[dx][dy] == 1){
+                            q.push({dx , dy});
+                            grid[dx][dy] = 2;
+                            flag = true;
                     }
                 }
             }
